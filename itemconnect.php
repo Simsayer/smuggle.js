5f73165681a5f50d46252a750ab861b3cc5bd512
Simsayer <simsayer@gmail.com>
2018-10-07 15:32:36 +0100

    itemconnect.php
    Just some rough outlines for your project

---
+++ itemconnect.php
@@ -0,0 +1,7 @@
+// connect to the database with item verification.
+
+<?php
+
+//mysql_connect(") Waiting on Monkman for server details.
+
+//if(table[0,] == 2) Compare array with item for calculations.
